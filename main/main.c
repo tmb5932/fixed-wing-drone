b10f79bb06b0e10e95a3562642c4711e21164159
tmb5932 <tmb5932@g.rit.edu>
2026-03-21 00:48:36 -0400

    Current setup can switch between manual pass-through and ignore input mode.

--- main/main.c
+++ main/main.c
@@ -7,80 +7,201 @@
 
 static const char *TAG = "MAIN";
 
-#define SERVO_PULSE_GPIO             21        // GPIO connects to the PWM signal line
 #define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
 #define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
-#define CAPTURE_RESOLUTION (1000000)
+#define CAPTURE_RESOLUTION (1000000) // This is unchangeable on the esp32s3, so its actually not this... its 80MHz
 
-// GPIO assignments for RC receiver inputs
-#define RC_THROTTLE_GPIO   10
-#define RC_LEFT_GPIO       11
-#define RC_RIGHT_GPIO      12
-#define RC_MODE_GPIO       13
+#define MCPWM_CAPTURE_CHANNELS_PER_GROUP (SOC_MCPWM_CAPTURE_TIMERS_PER_GROUP * SOC_MCPWM_CAPTURE_CHANNELS_PER_TIMER)
+#define MCPWM_TRIGGERS_PER_GROUP (SOC_MCPWM_OPERATORS_PER_GROUP * SOC_MCPWM_TRIGGERS_PER_OPERATOR)
+#define NUM_RC_CHANNELS (6)
 
+// GPIO assignments for RC receiver inputs (i swear these are 6 almost neighboring pins)
+#define CH1_IN_GPIO  21
+#define CH2_IN_GPIO  47
+#define CH3_IN_GPIO  48
+#define CH4_IN_GPIO  35
+#define CH5_IN_GPIO  36
+#define CH6_IN_GPIO  37
+
+
+// GPIO assignments for outputs to the peripherals
+#define CH1_OUT_GPIO  9
+#define CH2_OUT_GPIO  10
+#define CH3_OUT_GPIO  11
+#define CH4_OUT_GPIO  12
+#define CH5_OUT_GPIO  13
+#define CH6_OUT_GPIO  14
+
+// translating from channel numbers to what it controls
+#define CH1_NUM 1
+#define CH2_NUM 2
+#define CH3_NUM 3
+#define CH4_NUM 4
+#define CH5_NUM 5
+#define CH6_NUM 6
+
+#define RC_THROTTLE (CH3_NUM - 1)
+#define RC_AILERON  (CH1_NUM - 1)
+#define RC_ELEVATOR (CH2_NUM - 1)
+#define RC_RUDDER   (CH4_NUM - 1)
+#define RC_SWITCH   (CH5_NUM - 1)
+#define RC_DIAL     (CH6_NUM - 1)
+
+// #define RC_THROTTLE_IN      CH3_IN_GPIO
+// #define RC_THROTTLE_OUT     CH3_OUT_GPIO
+
+// #define RC_AILERON_IN       CH1_IN_GPIO
+// #define RC_AILERON_OUT      CH1_OUT_GPIO
+
+// #define RC_ELEVATOR_IN      CH2_IN_GPIO
+// #define RC_ELEVATOR_OUT     CH2_OUT_GPIO
+
+// #define RC_RUDDER_IN        CH4_IN_GPIO
+// #define RC_RUDDER_OUT       CH4_OUT_GPIO
+
+// // switch and dial are input only
+// #define RC_AUX_SWITCH_IN    CH5_IN_GPIO
+// #define RC_AUX_DIAL_IN      CH6_IN_GPIO
+
+static output_group_t out_groups[SOC_MCPWM_GROUPS];
+static rc_capture_group_t cap_groups[SOC_MCPWM_GROUPS];
+
+bool autonomous_mode_enabled(uint32_t mode_us) {
+    return (mode_us < 1500);
+}
+
+
+/* INPUT CONVERSIONS (2 groups with 3 capture channels each) */
+/**
+ * Convert rc channel number to capture channel number
+ * The ch parameter should be 0-indexed (subtract 1 from it before passing in) 
+*/
+int rc_channel_to_capture_channel(int ch)
+{
+    if (ch < 0 || ch >= NUM_RC_CHANNELS) { ESP_LOGE(TAG, "Invalid channel number: %d; should be in [0, %d)", ch, NUM_RC_CHANNELS); abort(); }
+    return ch % MCPWM_CAPTURE_CHANNELS_PER_GROUP; 
+}
+
+/**
+ * Convert rc channel number to capture group number
+ * The ch parameter should be 0-indexed (subtract 1 from it before passing in) 
+*/
+int rc_channel_to_capture_group(int ch)
+{
+    if (ch < 0 || ch >= NUM_RC_CHANNELS) { ESP_LOGE(TAG, "Invalid channel number: %d; should be in [0, %d)", ch, NUM_RC_CHANNELS); abort(); }
+    return ch / MCPWM_CAPTURE_CHANNELS_PER_GROUP;
+}
+
+/* OUTPUT CONVERSIONS (2 groups with 3 operators each, with each operator having 2 triggers) */
+/**
+ * Convert rc channel number to output group number
+ * The ch parameter should be 0-indexed (subtract 1 from it before passing in) 
+*/
+int rc_channel_to_output_group(int ch)
+{
+    if (ch < 0 || ch >= NUM_RC_CHANNELS) { ESP_LOGE(TAG, "Invalid channel number: %d; should be in [0, %d)", ch, NUM_RC_CHANNELS); abort(); }
+    return ch / MCPWM_TRIGGERS_PER_GROUP;
+}
+
+/**
+ * Convert rc channel number to output operator number
+ * The ch parameter should be 0-indexed (subtract 1 from it before passing in) 
+*/
+int rc_channel_to_output_op(int ch)
+{
+    if (ch < 0 || ch >= NUM_RC_CHANNELS) { ESP_LOGE(TAG, "Invalid channel number: %d; should be in [0, %d)", ch, NUM_RC_CHANNELS); abort(); }
+    return ch / SOC_MCPWM_GENERATORS_PER_OPERATOR;
+}
+
+/**
+ * Convert rc channel number to output comparator / generator number
+ * The ch parameter should be 0-indexed (subtract 1 from it before passing in) 
+*/
+int rc_channel_to_output_cmpr(int ch)
+{
+    if (ch < 0 || ch >= NUM_RC_CHANNELS) { ESP_LOGE(TAG, "Invalid channel number: %d; should be in [0, %d)", ch, NUM_RC_CHANNELS); abort(); }
+    return ch % SOC_MCPWM_COMPARATORS_PER_OPERATOR;
+}
+
+void control_task(void *arg) {
+    while (1) {
+
+        uint32_t ch[NUM_RC_CHANNELS];
+        ch[RC_THROTTLE] = cap_groups[rc_channel_to_capture_group(RC_THROTTLE)].inputs[rc_channel_to_capture_channel(RC_THROTTLE)].pulse_width_us;
+        ch[RC_AILERON] = cap_groups[rc_channel_to_capture_group(RC_AILERON)].inputs[rc_channel_to_capture_channel(RC_AILERON)].pulse_width_us;
+        ch[RC_ELEVATOR] = cap_groups[rc_channel_to_capture_group(RC_ELEVATOR)].inputs[rc_channel_to_capture_channel(RC_ELEVATOR)].pulse_width_us;
+        ch[RC_RUDDER] = cap_groups[rc_channel_to_capture_group(RC_RUDDER)].inputs[rc_channel_to_capture_channel(RC_RUDDER)].pulse_width_us;
+        ESP_LOGI(TAG, "g%d; c%d", rc_channel_to_capture_group(RC_SWITCH), rc_channel_to_capture_channel(RC_SWITCH));
+        ch[RC_SWITCH] = cap_groups[rc_channel_to_capture_group(RC_SWITCH)].inputs[rc_channel_to_capture_channel(RC_SWITCH)].pulse_width_us;
+        // ch[RC_DIAL] = cap_groups[rc_channel_to_capture_group(RC_DIAL)].inputs[rc_channel_to_capture_channel(RC_DIAL)].pulse_width_us;
+
+
+        // if not heard from radio in like 100 ms or sometihnhg, go autonomous
+        if (autonomous_mode_enabled(ch[RC_SWITCH])) {
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_THROTTLE)].operators[rc_channel_to_output_op(RC_THROTTLE)].comparators[rc_channel_to_output_cmpr(RC_THROTTLE)], ch[RC_THROTTLE]);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_AILERON)].operators[rc_channel_to_output_op(RC_AILERON)].comparators[rc_channel_to_output_cmpr(RC_AILERON)], ch[RC_AILERON]);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_ELEVATOR)].operators[rc_channel_to_output_op(RC_ELEVATOR)].comparators[rc_channel_to_output_cmpr(RC_ELEVATOR)], ch[RC_ELEVATOR]);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_RUDDER)].operators[rc_channel_to_output_op(RC_RUDDER)].comparators[rc_channel_to_output_cmpr(RC_RUDDER)], ch[RC_RUDDER]);
+            // update_comparator_value(out_groups[rc_channel_to_output_group(5)].operators[rc_channel_to_output_op(5)].comparators[rc_channel_to_output_cmpr(5)], ch5);
+            // update_comparator_value(out_groups[rc_channel_to_output_group(6)].operators[rc_channel_to_output_op(6)].comparators[rc_channel_to_output_cmpr(6)], ch6);
+        } else {
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_THROTTLE)].operators[rc_channel_to_output_op(RC_THROTTLE)].comparators[rc_channel_to_output_cmpr(RC_THROTTLE)], 1500);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_AILERON)].operators[rc_channel_to_output_op(RC_AILERON)].comparators[rc_channel_to_output_cmpr(RC_AILERON)], 1500);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_ELEVATOR)].operators[rc_channel_to_output_op(RC_ELEVATOR)].comparators[rc_channel_to_output_cmpr(RC_ELEVATOR)], 1500);
+            update_comparator_value(out_groups[rc_channel_to_output_group(RC_RUDDER)].operators[rc_channel_to_output_op(RC_RUDDER)].comparators[rc_channel_to_output_cmpr(RC_RUDDER)], 1500);
+            // update_comparator_value(out_groups[rc_channel_to_output_group(5)].operators[rc_channel_to_output_op(5)].comparators[rc_channel_to_output_cmpr(5)], 1500);
+            // update_comparator_value(out_groups[rc_channel_to_output_group(6)].operators[rc_channel_to_output_op(6)].comparators[rc_channel_to_output_cmpr(6)], 1500);
+        }
+        
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
+}
 
 void app_main(void)
 {
     // OUTPUT SETUP
     ESP_LOGI(TAG, "Creating output group 0.");
-    group_t g0 = create_group(0);
+    out_groups[0] = create_group(0);
 
     mcpwm_timer_handle_t timer0 = NULL;
     initialize_timer(&timer0, 0, SERVO_TIMEBASE_RESOLUTION_HZ, SERVO_TIMEBASE_PERIOD);
 
     ESP_LOGI(TAG, "Adding operator and generator/comparator.");
-    int op_idx = add_operator(&g0, timer0);
+    add_operator(&out_groups[0], 0, timer0);
+    add_gen_cmpr(&out_groups[0].operators[0], 0, CH1_OUT_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
+    add_gen_cmpr(&out_groups[0].operators[0], 1, CH2_OUT_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
 
-    int gc_idx1 = add_gen_cmpr(&g0.operators[op_idx], SERVO_PULSE_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
-    int gc_idx2 = add_gen_cmpr(&g0.operators[op_idx], 3, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
-
+    add_operator(&out_groups[0], 1, timer0);
+    add_gen_cmpr(&out_groups[0].operators[1], 0, CH3_OUT_GPIO, MOTOR_TYPE, MIN_STARTING_VALUE);    
+    add_gen_cmpr(&out_groups[0].operators[1], 1, CH4_OUT_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
+    
+    // add_operator(&out_groups[0], 2, timer0);
+    // add_gen_cmpr(&out_groups[0].operators[2], 0, CH5_OUT_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
+    // add_gen_cmpr(&out_groups[0].operators[2], 1, CH6_OUT_GPIO, SERVO_TYPE, DEFAULT_STARTING_VALUE);    
     mcpwm_timer_start(&timer0);
 
     // INPUT SETUP
-
     ESP_LOGI(TAG, "Creating capture group 0.");
-    rc_capture_group_t cap0 = create_rc_capture_group(0);
-    rc_capture_init_timer(&cap0, CAPTURE_RESOLUTION);
+    cap_groups[0] = create_rc_capture_group(0);
+    rc_capture_init_timer(&cap_groups[0], CAPTURE_RESOLUTION); // Capture resolution not used on esp32s3
 
-    rc_capture_add_channel(&cap0, 0, RC_THROTTLE_GPIO);
-    rc_capture_add_channel(&cap0, 1, RC_LEFT_GPIO);
-    rc_capture_add_channel(&cap0, 2, RC_RIGHT_GPIO);
+    rc_capture_add_channel(&cap_groups[0], 0, CH1_IN_GPIO);
+    rc_capture_add_channel(&cap_groups[0], 1, CH2_IN_GPIO);
+    rc_capture_add_channel(&cap_groups[0], 2, CH3_IN_GPIO);
 
-    rc_capture_start(&cap0);
-
+    rc_capture_start(&cap_groups[0]);
 
     ESP_LOGI(TAG, "Creating capture group 1.");
-    rc_capture_group_t cap1 = create_rc_capture_group(1);
-    rc_capture_init_timer(&cap1, CAPTURE_RESOLUTION);
+    cap_groups[1] = create_rc_capture_group(1);
+    rc_capture_init_timer(&cap_groups[1], CAPTURE_RESOLUTION); // Capture resolution not used on esp32s3
 
-    rc_capture_add_channel(&cap1, 0, RC_MODE_GPIO);
-    
-    rc_capture_start(&cap1);
+    rc_capture_add_channel(&cap_groups[1], 0, CH4_IN_GPIO);
+    rc_capture_add_channel(&cap_groups[1], 1, CH5_IN_GPIO);
+    rc_capture_add_channel(&cap_groups[1], 2, CH6_IN_GPIO);
 
-    int a1 = 0;
+    rc_capture_start(&cap_groups[1]);
 
-    while (1) {
-        uint32_t p1 = servo_angle_to_compare(a1);
-        uint32_t p2 = servo_angle_to_compare(-a1);
-
-        update_comparator_value(g0.operators[op_idx].comparators[gc_idx1], p1);
-        update_comparator_value(g0.operators[op_idx].comparators[gc_idx2], p2);
-
-        a1 = (a1 == 25) ? -25 : 25;
-
-        uint32_t throttle = cap0.inputs[0].pulse_width_us;
-        uint32_t left     = cap0.inputs[1].pulse_width_us;
-        uint32_t right    = cap0.inputs[2].pulse_width_us;
-        uint32_t mode     = cap1.inputs[0].pulse_width_us;
-
-        ESP_LOGI(TAG,
-                 "thr=%lu us | left=%lu us | right=%lu us | mode=%lu us",
-                 (unsigned long)throttle,
-                 (unsigned long)left,
-                 (unsigned long)right,
-                 (unsigned long)mode);
-
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+    xTaskCreatePinnedToCore(control_task, "control", 8096, NULL, 1, NULL, 1);
+    return;
 }
